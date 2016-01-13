1fdd50cd460cf59930d69a271d187c589528e54a
Ethan Riback <rethack24@gmail.com>
2016-01-13 16:30:20 -0500

    Begin working on login implementation. The following was added: -Home screen with no functionality accept it has a button to pop the login view. -LoginViewController which will now help the Login function. -SignUpViewController which will be used to implement account creation. UI is "Under Construction", and only provides a way to return to the LoginView. -ForgotPasswordViewController, which will implement password retrieval. UI is "Under Construction", and only provides a way to return to the LoginView.
    Currently, the video functionality of JamJar is not accessible.

---
+++ JamJar/LoginViewController.swift
@@ -0,0 +1,41 @@
+//
+//  LoginViewController.swift
+//  JamJar
+//
+//  Created by Ethan Riback on 1/13/16.
+//  Copyright © 2016 JamJar. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class LoginViewController: UIViewController{
+    
+    @IBOutlet var usernameTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        //Looks for single or multiple taps.
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "dismissKeyboard")
+        view.addGestureRecognizer(tap)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    //Calls this function when the tap is recognized.
+    func dismissKeyboard() {
+        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+        view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        return false
+    }
+}
---
+++ JamJar/SignUpViewController.swift
@@ -0,0 +1,43 @@
+//
+//  SignUpViewController.swift
+//  JamJar
+//
+//  Created by Ethan Riback on 1/13/16.
+//  Copyright © 2016 JamJar. All rights reserved.
+//
+
+import UIKit
+
+import Alamofire
+
+class SignUpViewController: UIViewController{
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        //Looks for single or multiple taps.
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "dismissKeyboard")
+        view.addGestureRecognizer(tap)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    //Calls this function when the tap is recognized.
+    func dismissKeyboard() {
+        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+        view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        return false
+    }
+    
+    @IBAction func returnToLoginButtonPressed(sender: UIButton) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+}
