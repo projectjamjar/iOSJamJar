1f968542b7cf119e493372e04e6f06b2275ff9a5
Ethan Riback <rethack24@gmail.com>
2016-02-13 17:46:52 -0500

    Began setting up UploadVideo View.

---
+++ JamJar/UploadVideoController.swift
@@ -0,0 +1,16 @@
+//
+//  UploadVideoController.swift
+//  JamJar
+//
+//  Created by Ethan Riback on 2/13/16.
+//  Copyright © 2016 JamJar. All rights reserved.
+//
+
+import UIKit
+
+class UploadVideoViewController: BaseViewController {
+    
+    @IBAction func continueButtonPressed(sender: UIButton) {
+        print("Pick video to upload");
+    }
+}
