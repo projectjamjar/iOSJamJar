4cc37f6260c8278d72fd7783b78f38a9efc91fe8
Ethan Riback <rethack24@gmail.com>
2016-02-18 14:07:52 -0500

    Added AutoCompleteTextField and using it for UploadVideo results for Artists and Venues. Added an artist text field and it gives result suggestions.

--- JamJar/UploadVideoController.swift
+++ JamJar/UploadVideoController.swift
@@ -10,6 +10,41 @@ import UIKit
 
 class UploadVideoViewController: BaseViewController {
     
+    @IBOutlet var artistsTextField: AutoCompleteTextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        artistsTextField.onTextChange = {[weak self] text in
+            // your code goes here
+            print(self?.artistsTextField.text);
+            APIService.get(APIService.buildURL("artists/search/" + (self?.artistsTextField.text)!)).response{request, response, data, error in
+                
+                let artistsData = try! NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.MutableContainers)
+                
+                //Variable to store results
+                //TODO: Store more than names and have the results display names
+                var artistResults = [String]()
+                
+                //print(artistsData)
+                
+                if let artists = artistsData as? NSArray{
+                    for artist in artists {
+                        print(artist["name"])
+                        artistResults.append(artist["name"] as! String)
+                    }
+                    //print(artists[0])
+                    //print("artists worked")
+                    self?.artistsTextField.autoCompleteStrings = artistResults
+                }
+                else {
+                    self?.artistsTextField.autoCompleteStrings = nil
+                }
+            }
+        }
+    }
+    
     @IBAction func continueButtonPressed(sender: UIButton) {
         print("Pick video to upload");
         
