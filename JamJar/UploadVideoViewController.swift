0cd77f2301795a763a90ba87c43ef8c882ba842d
Ethan Riback <rethack24@gmail.com>
2016-02-24 22:47:24 -0500

    On UploadVideoViewController, now able to switch between the videos.

--- JamJar/UploadVideoViewController.swift
+++ JamJar/UploadVideoViewController.swift
@@ -12,6 +12,7 @@ import AVFoundation
 
 class UploadVideoViewController: BaseViewController{
     
+    var currentVideoSelected = 0
     var selectedArtists = [Artist]()
     var selectedVenue: Venue!
     var videosToUpload: [AnyObject]?
@@ -25,6 +26,10 @@ class UploadVideoViewController: BaseViewController{
         //print(selectedVenue.description)
         //print(self.videoToUpload)
         
+        //placeholder text for videoNameTextField needs to be set to white
+        videoNameTextField.attributedPlaceholder = NSAttributedString(string:"Video Name",
+            attributes:[NSForegroundColorAttributeName: UIColor.whiteColor()])
+        
         //removes the white background from the corners to make the UI look better
         self.publicPrivateSegmentedControl.layer.cornerRadius = 5.0;
     }
@@ -32,6 +37,31 @@ class UploadVideoViewController: BaseViewController{
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func changeVideo(newIndex: Int) {
+        print("Change Video")
+        let embeddedVideoViewController = self.childViewControllers[0] as! AVPlayerViewController
+        
+        let videoPath = self.videosToUpload![currentVideoSelected]["UIImagePickerControllerReferenceURL"]
+        
+        embeddedVideoViewController.player = AVPlayer(URL: videoPath as! NSURL)
+    }
+    
+    @IBAction func leftButtonPressed(sender: UIButton) {
+        currentVideoSelected--
+        if(currentVideoSelected < 0) {
+            currentVideoSelected = (videosToUpload?.count)! - 1
+        }
+        changeVideo(currentVideoSelected)
+    }
+    
+    @IBAction func rightButtonPressed(sender: UIButton) {
+        currentVideoSelected++
+        if(currentVideoSelected >= videosToUpload?.count) {
+            currentVideoSelected = 0
+        }
+        changeVideo(currentVideoSelected)
     }
     
     @IBAction func uploadVideos(sender: UIButton) {
@@ -44,7 +74,7 @@ class UploadVideoViewController: BaseViewController{
             print(self.videosToUpload)
             let embeddedVideoViewController = segue.destinationViewController as! AVPlayerViewController
             
-            let videoPath = self.videosToUpload![0]["UIImagePickerControllerReferenceURL"]
+            let videoPath = self.videosToUpload![currentVideoSelected]["UIImagePickerControllerReferenceURL"]
             print(videoPath)
             //let videoURL = NSURL(fileURLWithPath: videoPath)
             //print(videoURL)
