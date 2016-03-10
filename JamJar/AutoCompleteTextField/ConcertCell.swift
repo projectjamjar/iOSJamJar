6268f3e82eb90e995dda54b1e481a091e4228171
Mark Koh <markkohdev@gmail.com>
2016-03-10 00:41:28 -0500

    Recommitting I guess or something.

--- JamJar/AutoCompleteTextField/ConcertCell.swift
+++ JamJar/AutoCompleteTextField/ConcertCell.swift
@@ -6,4 +6,11 @@
 //  Copyright © 2016 JamJar. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class ConcertCell: UITableViewCell {
+    @IBOutlet weak var thumbnailImageView: UIImageView!
+    @IBOutlet weak var artistLabel: UILabel!
+    @IBOutlet weak var dateVenueLabel: UILabel!
+    @IBOutlet weak var videoCountLabel: UILabel!
+}
--- JamJar/DiscoverViewController.swift
+++ JamJar/DiscoverViewController.swift
@@ -8,29 +8,15 @@
 
 import UIKit
 
-class ConcertCell: UITableViewCell {
-    @IBOutlet weak var thumbnailImageView: UIImageView!
-    @IBOutlet weak var artistLabel: UILabel!
-    @IBOutlet weak var dateLabel: UILabel!
-    @IBOutlet weak var videoCountLabel: UILabel!
-}
-
 class DiscoverViewController: BaseViewController {
-    @IBOutlet weak var sectionPicker: UISegmentedControl!
     
+    @IBOutlet weak var sectionPicker: UISegmentedControl!
     @IBOutlet weak var tableView: UITableView!
+    
+//    var concerts = [Concert]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-    @IBAction func logoutButtonPressed(sender: UIButton) {
-        UserService.logout()
-        //        self.dismissViewControllerAnimated(true, completion: nil)
-    }
 }
