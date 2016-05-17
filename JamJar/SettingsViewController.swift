20b3ed177226cd9eb51317753be45a2e085294cb
Ethan Riback <rethack24@gmail.com>
2016-05-17 01:17:28 -0400

    Unblocking shows success view.

--- JamJar/SettingsViewController.swift
+++ JamJar/SettingsViewController.swift
@@ -62,6 +62,7 @@ class SettingsViewController: BaseViewController{
                 // Remove that kid
                 self.blockedUsersStackView.removeArrangedSubview(label)
                 label.removeFromSuperview()
+                showSuccessView()
             })
         }
     }
